f9e36298099fc5213f6d1a5a5da07af5ea3c4807
Muhammad <muhammad_bw_26@mail.ru>
2023-09-27 15:31:23 +0500

    maket done

--- scripts/Lang_Changer.js
+++ scripts/Lang_Changer.js
@@ -1,23 +1,66 @@
 import { language } from "./language.js";
 
 window.addEventListener("DOMContentLoaded", () => {
+    // lang btn
+    let langs = document.querySelectorAll('.Site_Language button');
 
+    // Nav
     let Nav = document.querySelectorAll('.navbar-nav li a');
 
+    // Hero section
     let logo_main_text = document.querySelector('.Hero div div p');
-    let langs = document.querySelectorAll('.Site_Language button');
-    let apply_btn = document.querySelector(".Hero div div div button")
+    let apply_btn = document.querySelector(".Hero div div div button");
 
-    const LangChangeHandle = (selected_lang)=>{
+    // Curse section
+    let CurseHeader = document.querySelector('#CurseHeader');
+    let CurseHeaders = document.querySelectorAll('#CurseCard h3');
+    let Medlist = document.querySelectorAll('.MedicineCurse ul li');
+    let Sublist = document.querySelectorAll('.SubcurseCurse ul li');
+    let LangCurse = document.querySelectorAll('.LanguageCurse ul li');
 
-            Nav.forEach((element, index) =>{
-                element.innerHTML = language[selected_lang].NAV[index];
-            })
-            
-            logo_main_text.innerHTML = language[selected_lang].main[0];
-            apply_btn.innerHTML = language[selected_lang].NAV[4]
+    //About section
+
+    let h_about = document.getElementById('h_about');
+
+
+    const LangChangeHandle = (selected_lang) => {
+
+        // Navigation
+        Nav.forEach((element, index) => {
+            element.innerHTML = language[selected_lang].NAV[index];
+        })
+
+        // Logo and Apply btns
+        logo_main_text.innerHTML = language[selected_lang].main[0];
+        apply_btn.innerHTML = language[selected_lang].NAV[3]
+
+        ////////// Curse //////////
+        // head
+        CurseHeader.innerHTML = language[selected_lang].NAV[1];
+
+        CurseHeaders.forEach((element, index) => {
+            element.innerHTML = language[selected_lang].Curse[index][0];
+        });
+
+        Medlist.forEach((element, index) =>{
+            element.innerHTML = language[selected_lang].Curse[0][index+1];
+        })
+
+        Sublist.forEach((element, index) =>{
+            element.innerHTML = language[selected_lang].Curse[1][index+1];
+        })
+
+        LangCurse.forEach((element, index) =>{
+            element.innerHTML = language[selected_lang].Curse[2][index+1];
+        })
+
+        // About
+        h_about.innerHTML = language[selected_lang].NAV[2];
+
+
+
     }
-    
+
 
     Array.from(langs).map((lang) => {
         lang.addEventListener('click', () => {
